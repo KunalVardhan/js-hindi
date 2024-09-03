dee743a3112b0853c67809939eacf79c60759d6a
KunalVardhan <121881533+KunalVardhan@users.noreply.github.com>
2024-09-03 16:59:00 +0000

    data type

---
+++ 01_basics/variablr.js
@@ -0,0 +1,20 @@
+const accountId=13546
+let accountemail="kunalvardhn709gmail.com"
+var accountpassword="2546"
+accountcity="agra"
+let account_syate;
+
+// accountId=2 not allowd
+
+/*
+prefter not use var  becacuse of issue in block scope and fuctional scope 
+*/
+
+console.log(accountId);
+
+accountemail="kunal@al"
+accountpassword="3565"
+accountcity="mathura"
+
+console.log(accountemail);
+console.table([accountId,accountemail,accountpassword,accountcity,account_syate])
