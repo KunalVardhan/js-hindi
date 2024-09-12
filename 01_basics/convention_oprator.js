0ddce8ef05843a2b2d543f8b4bf275a1251725bf
KunalVardhan <121881533+KunalVardhan@users.noreply.github.com>
2024-09-12 08:15:56 +0000

    conversion of data type

---
+++ 01_basics/convention_oprator.js
@@ -0,0 +1,23 @@
+let score ="33"
+
+//console.log(typeof score);
+//console.log(typeof score);
+
+let value=Number(score);
+//console.log(typeof value);
+
+// 33 => converted esily= 33
+//33abc=> NAN (not a number )
+
+// true=1,,,false=0
+let log=1
+let booleanISLoggedIN= Boolean(log)
+console.log(booleanISLoggedIN);
+
+// 1=>true 0=>false
+//"" => faslse
+//"kunal"= true
+
+let somenumber=33
+let steingnumber= String(somenumber);
+console.log(typeof steingnumber);
