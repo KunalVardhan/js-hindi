79225b26c55d5fdc109efc8d7c30ec2c9eb4f157
KunalVardhan <121881533+KunalVardhan@users.noreply.github.com>
2024-09-15 14:15:39 +0000

    rr

---
+++ 02_basics/01.arrays.js
@@ -0,0 +1,16 @@
+const myarr=[0,1,2,3,5,6]
+const myHeros=["spiderman", "Caption_america", "thor"]
+
+console.log();
+myarr.push(7)
+console.log(myarr);
+//myArr.unshift(9)
+//myArr.shift()
+
+//// slice splice
+ const mynum1 =myarr.slice(1,3)
+ const mynum2 =myarr.splice(1,3)
+
+
+ console.log(mynum1);
+ console.log(mynum2);
